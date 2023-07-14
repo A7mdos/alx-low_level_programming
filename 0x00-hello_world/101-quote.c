4e69131b14d4a89a72dbe4d95b6d7dfa0b631447
A7mdos <a7mdos1999@gmail.com>
2023-07-14 03:33:45 +0200

    Advanced tasks - shell script + C program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+
+/**
+ * main - Prints a string to standard error.
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	char message[59] = "and that piece of art is useful\" - \
+						Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 59);
+
+	return (1);
+}
