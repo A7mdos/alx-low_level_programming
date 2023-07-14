c379780391e9dc914c4ff8d6dac4310a721ff21c
A7mdos <a7mdos1999@gmail.com>
2023-07-14 03:42:17 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-	char message[60] = "and that piece of art is useful\" - \
+	char message[59] = "and that piece of art is useful\" - \
 Dora Korpar, 2015-10-19\n";
 
-	write(2, message, 60);
+	write(2, message, 59);
 
 	return (1);
 }
