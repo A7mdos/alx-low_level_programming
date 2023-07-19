5e36d0416c13214bb8799c7f13e38c081dd1eb91
A7mdos <a7mdos1999@gmail.com>
2023-07-19 04:12:20 +0200

    Updated 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -38,9 +38,9 @@ int main(void)
 			left_half += 1;
 		}
 		printf("%lu%lu", left_half, right_half);
-		if (i == 98)
-			break;
-		printf(", ");
+		if (i != 98)
+			printf(", ");
+		
 		first_left_half = second_left_half;
 		first_right_half = second_right_half;
 		second_left_half = left_half;
