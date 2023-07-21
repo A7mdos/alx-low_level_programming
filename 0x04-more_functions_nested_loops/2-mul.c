34028b775d1d93db0dfd777c2603ae5dc2f5c958
A7mdos <a7mdos1999@gmail.com>
2023-07-21 02:26:51 +0200

    All mandatory tasks - C programs and main.h header file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: the first integer to be multiplied
+ * @b: the second integer to be multiplied
+ *
+ * Return: the multiple of the 2 integers
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
