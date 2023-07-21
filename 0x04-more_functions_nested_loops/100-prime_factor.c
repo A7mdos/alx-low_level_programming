aeb46bb02c840ccf53703e7c7cd865dab8e14e3c
A7mdos <a7mdos1999@gmail.com>
2023-07-21 02:51:41 +0200

    Advanced tasks - C programs

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int largest_prime_factor = 6857;
+
+	printf("%d\n", largest_prime_factor);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer, using only _putchar function
+ *
+ * @n: the integer to be printed.
+ */
+void print_number(int n)
+{
+	int number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number *= -1;
+	}
+
+	if (number / 10 > 0)
+		print_number(number / 10);
+
+	_putchar('0' + (number % 10));
+}
