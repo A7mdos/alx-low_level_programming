c728705c66b641c052b8bbb7a0be7c7217b2e416
A7mdos <a7mdos1999@gmail.com>
2023-08-25 04:33:55 +0200

    Hellooo, Holberton!

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,13 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0  ; Format string for printf: %s (string) followed by newline
-
-section .text
-    global main
-    extern printf
+	global main
+	extern printf
 
 main:
-    push rbp
-    mov rdi, format    ; First argument: Format string
-    mov rsi, hello     ; Second argument: Pointer to the string "Hello, Holberton"
-    call printf        ; Call printf function
-    add rsp, 8         ; Clean up the stack
-    pop rbp
-    ret
+
+	mov rdi, hello
+	xor rax, rax
+	call printf
+
+	xor rax, rax
+	ret
+
+hello: db `Hello, Holberton`, 10
